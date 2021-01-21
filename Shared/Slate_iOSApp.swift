dfe23303fbd79d8ca613b9a0b02eb3dfe5a74737
Martina <martinalong10@gmail.com>
2021-01-21 14:36:18 -0800

    Initial Commit

---
+++ Shared/Slate_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slate_iOSApp.swift
+//  Shared
+//
+//  Created by Martina Long on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Slate_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
